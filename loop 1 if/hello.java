846b8fb473c33609a808ba0cbc652f10b0753765
Nashon Ernest <92583323+ernestnash@users.noreply.github.com>
2022-02-01 14:55:21 +0300

    Add files via upload

---
+++ loop 1 if/hello.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package nashon;
+
+/**
+ * @author NASHON ERNEST
+ *
+ */
+import java.util.Scanner;
+public class hello {
+
+	/**
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		
+		System.out.println("Write something...");
+		//prints onto the screen
+		
+		try (Scanner scanner = new Scanner(System.in)) {
+			
+			String j=" ";
+			//declaring a variable j which is a string
+			
+			while(!j.equals("quit"))/*loops until user types quit*/{
+				
+				System.out.print("input: ");
+				
+				j = scanner.next().toLowerCase();
+				
+				if(!j.equals("quit"))
+					//stops the program from echoing the quit statement
+					
+					System.out.println(j);
+			}	
+		}
+	}
+
+}
