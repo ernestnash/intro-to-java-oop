f6416662c582b0074f608baee189ac58905caec1
Nashon Ernest <92583323+ernestnash@users.noreply.github.com>
2022-02-01 14:54:13 +0300

    Add files via upload

---
+++ shirt on or off/main.java
@@ -0,0 +1,12 @@
+package nashon;
+
+public class main {
+
+	public static void main(String[] args) {
+		shirt s = new shirt();
+		s.putOn();
+		
+		
+	}
+
+}
---
+++ shirt on or off/shirt.java
@@ -0,0 +1,19 @@
+package nashon;
+
+public class shirt {
+	public String color;
+	public char size;
+	
+	shirt(){
+		
+	}
+	public static void putOn() {
+		System.out.println("Shirt is on");
+		
+	}
+	public static void takeOff() {
+		System.out.println("Shirt is off");
+		
+	}
+	
+}
